d5a5201196a9d9d9195989a288d8817e558dacb7
wandavenus <75023651+wandavenus@users.noreply.github.com>
2026-06-03 03:41:44 +0700

    Update music_list.dart

--- lib/pages/music_list.dart
+++ lib/pages/music_list.dart
@@ -134,77 +134,7 @@ Future<void> loadSongs() async {
         },
   
       ),
- bottomNavigationBar: AudioService.currentSong != null
-    ? Container(
-        height: 70,
-        margin: const EdgeInsets.all(8),
-        padding: const EdgeInsets.symmetric(horizontal: 12),
-        decoration: BoxDecoration(
-          color: Colors.grey.shade900,
-          borderRadius: BorderRadius.circular(15),
-        ),
-        child: Row(
-          children: [
-            QueryArtworkWidget(
-              controller: audioQuery,
-              id: AudioService.currentSong!.id,
-              type: ArtworkType.AUDIO,
-              artworkWidth: 50,
-              artworkHeight: 50,
-              nullArtworkWidget: const Icon(
-                Icons.music_note,
-                color: Colors.white,
-              ),
-            ),
-
-            const SizedBox(width: 10),
-
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    AudioService.currentSong!.title,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: const TextStyle(color: Colors.white),
-                  ),
-
-                  Text(
-                    AudioService.currentSong!.artist ??
-                        "Unknown Artist",
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: const TextStyle(
-                      color: Colors.white70,
-                      fontSize: 12,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            IconButton(
-              onPressed: () async {
-                if (AudioService.player.playing) {
-                  await AudioService.player.pause();
-                } else {
-                  await AudioService.player.play();
-                }
-
-                setState(() {});
-              },
-              icon: Icon(
-                AudioService.player.playing
-                    ? Icons.pause
-                    : Icons.play_arrow,
-                color: Colors.white,
-              ),
-            ),
-          ],
-        ),
-      )
-    : null,
+ 
     );
   }
 }
