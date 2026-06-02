c03b4b68bb5730ba1c57afc3918f65fb0700e77e
wandavenus <75023651+wandavenus@users.noreply.github.com>
2026-06-02 18:08:29 +0700

    Add currentSong property to AudioService

--- lib/services/audio_service.dart
+++ lib/services/audio_service.dart
@@ -1,5 +1,8 @@
 import 'package:just_audio/just_audio.dart';
+import 'package:on_audio_query/on_audio_query.dart';
 
 class AudioService {
   static final AudioPlayer player = AudioPlayer();
+
+  static SongModel? currentSong;
 }
