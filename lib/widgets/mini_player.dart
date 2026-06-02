7699dba730541ca818fe3cae0c94837ead9aa5af
wandavenus <75023651+wandavenus@users.noreply.github.com>
2026-06-03 02:27:50 +0700

    Create lib/widgets/mini_player.dart

---
+++ lib/widgets/mini_player.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MiniPlayer extends StatelessWidget {
+  const MiniPlayer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox();
+  }
+}
