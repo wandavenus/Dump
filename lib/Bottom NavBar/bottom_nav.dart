864cf73fbcacbba45b4c7e234f7c15ddb64fff83
wandavenus <75023651+wandavenus@users.noreply.github.com>
2026-06-03 18:11:54 +0700

    Update bottom_nav.dart

--- lib/Bottom NavBar/bottom_nav.dart
+++ lib/Bottom NavBar/bottom_nav.dart
@@ -52,7 +52,7 @@ Container(
   color: const Color(0xFF38383A),
 ),
     SizedBox(
-        height: 70,
+        height: 75,
 
         // padding: EdgeInsets.only(bottom: 0),
         // color: Colors.amber,
