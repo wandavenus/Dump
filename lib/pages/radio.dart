0e8567b40d2c90bcd97389c062dcd84eba545b0f
wandavenus <75023651+wandavenus@users.noreply.github.com>
2026-06-06 09:04:38 +0700

    Update radio.dart

--- lib/pages/radio.dart
+++ lib/pages/radio.dart
@@ -19,26 +19,26 @@ double _scrollOffset = 0;
       'text2': 'Songs you know and love',
       "topMsg": "New Release",
       "image": "https://static.stereogum.com/uploads/2023/03/LDR-Tunnel-1679672318-1000x1000.jpg",
-      "title": "Ocean Blvd",
-      "artist": "Lana Del Rey",
+      "title": "On My Way",
+      "artist": "Alan Walker",
       "song": ""
     },
     {
       'text1': 'Music Country',
       'text2': 'Where it sounds like home',
-      "topMsg": "Featuring Tame Impala",
+      "topMsg": "The Legend",
       "image": "https://qodeinteractive.com/magazine/wp-content/uploads/2020/06/16-Tame-Impala.jpg",
-      "title": "Tame Impala",
-      "artist": "Currents",
+      "title": "Hybrid Theory",
+      "artist": "Linkin Park",
       "song": ""
     },
     {
-      'text1': 'Music Country',
+      'text1': 'Music Jdm',
       'text2': 'Where it sounds like home',
       "topMsg": "Made for you",
       "image": "https://t2.gstatic.com/licensed-image?q=tbn:ANd9GcT9kry1myZTi2dMJ7OtgJjmdT__lImpI-pJ9mdq42Cz8HhIet_ro_Obp6q4xbksBbpT",
-      "title": "The dark side of the moon",
-      "artist": "Pink floyd ",
+      "title": "Alone",
+      "artist": "Marsmello",
       "song": ""
     },
     
