13f8018a7d344ec73d9fdd620a09a020705e33ba
wandavenus <75023651+wandavenus@users.noreply.github.com>
2026-06-07 11:30:41 +0700

    Update library_page.dart

--- lib/pages/library_page.dart
+++ lib/pages/library_page.dart
@@ -18,6 +18,8 @@ double _scrollOffset = 0;
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+  backgroundColor: Colors.blue,
+)
        automaticallyImplyLeading: false,
 backgroundColor: Colors.black,
 surfaceTintColor: Colors.transparent, 
