b4b5b010036941ef153636434aed831174088f2f
wandavenus <75023651+wandavenus@users.noreply.github.com>
2026-06-07 19:16:29 +0700

    Add SharedPreferences theme persistence controller

--- lib/themes/theme_controller.dart
+++ lib/themes/theme_controller.dart
@@ -1,5 +1,27 @@
 import 'package:flutter/foundation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeController {
-  static final ValueNotifier<bool> glassTheme = ValueNotifier(false);
+  static final ValueNotifier<bool> glassTheme =
+      ValueNotifier(false);
+
+  static Future<void> init() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    glassTheme.value =
+        prefs.getBool('glass_theme') ?? false;
+  }
+
+  static Future<void> setGlassTheme(
+    bool value,
+  ) async {
+    glassTheme.value = value;
+
+    final prefs = await SharedPreferences.getInstance();
+
+    await prefs.setBool(
+      'glass_theme',
+      value,
+    );
+  }
 }
