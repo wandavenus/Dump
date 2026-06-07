6652c30aa0176358f5b0e50ea2d835a4a34c9314
wandavenus <75023651+wandavenus@users.noreply.github.com>
2026-06-07 10:03:35 +0700

    Update app_icon_button.dart

--- lib/widgets/app_icon_button.dart
+++ lib/widgets/app_icon_button.dart
@@ -4,19 +4,15 @@ class AppIconButton extends StatelessWidget {
   final IconData icon;
   final VoidCallback onPressed;
   final Color? backgroundColor;
-  final Color? iconColor;
-  final double? size;
-  final double? iconSize;
+  final double iconSize;
 
   const AppIconButton({
-    Key? key,
+    super.key,
     required this.icon,
     required this.onPressed,
     this.backgroundColor,
-    this.iconColor,
-    this.size = 50,
     this.iconSize = 20,
-  }) : super(key: key);
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -24,14 +20,15 @@ class AppIconButton extends StatelessWidget {
       padding: const EdgeInsets.all(5),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(150),
-        color: backgroundColor ?? const Color.fromARGB(85, 158, 158, 158),
+        color: backgroundColor ??
+            const Color.fromARGB(85, 158, 158, 158),
       ),
       child: GestureDetector(
         onTap: onPressed,
         child: Icon(
           icon,
           size: iconSize,
-          color: iconColor ?? Colors.white,
+          color: const Color(0xFFF92D48),
         ),
       ),
     );
