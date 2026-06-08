72c68e6be08d3c8fbf5787ed2563e6fe229b97b7
wandavenus <75023651+wandavenus@users.noreply.github.com>
2026-06-08 17:25:00 +0700

    Sesuaikan tinggi navbar untuk pengujian gesture Xiaomi

--- lib/Bottom NavBar/bottom_nav.dart
+++ lib/Bottom NavBar/bottom_nav.dart
@@ -71,7 +71,7 @@ class _FirstPageState extends State<FirstPage> {
         );
 
         return Scaffold(
-          extendBody: false,
+          extendBody: true,
           body: IndexedStack(
             index: _selected_index,
             children: _pages.cast<Widget>(),
@@ -86,7 +86,7 @@ class _FirstPageState extends State<FirstPage> {
                   color: const Color(0xFF38383A),
                 ),
               SizedBox(
-                height: 70,
+                height: kBottomNavigationBarHeight,
                 child: isGlass ? GlassNavBar(child: navBar) : navBar,
               ),
             ],
