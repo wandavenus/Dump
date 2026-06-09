bcc41e4741fdbe02ad155785b21780609c7d0b67
wandavenus <75023651+wandavenus@users.noreply.github.com>
2026-06-10 05:53:44 +0700

    revert: remove albumId debug subtitle

--- lib/pages/music_list.dart
+++ lib/pages/music_list.dart
@@ -124,7 +124,7 @@ class _MusicListState extends State<MusicList> {
                     ),
                   ),
                   subtitle: Text(
-                    '${song.artist} | albumId=${song.albumId}',
+                    song.artist,
                     maxLines: 1,
                     overflow: TextOverflow.ellipsis,
                   ),
