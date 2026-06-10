55884d43399f5ad1a1446e6f9a05abea6218c6d2
wandavenus <75023651+wandavenus@users.noreply.github.com>
2026-06-10 08:38:51 +0700

    Update mini_player.dart

--- lib/widgets/mini_player.dart
+++ lib/widgets/mini_player.dart
@@ -52,7 +52,7 @@ class _MiniPlayerBody extends StatelessWidget {
                 Hero(
                   tag: PlayerHeroTags.artwork(song),
                   child: SongArtwork(
-                    albumId: song.songId,
+                    albumId: song.id,
                     size: 46,
                     borderRadius: BorderRadius.circular(3),
                   ),
