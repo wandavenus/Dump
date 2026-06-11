44acb82ebccc6eda15171cf49f25760a30edeb2f
wandavenus <75023651+wandavenus@users.noreply.github.com>
2026-06-12 06:46:19 +0700

    Add initial PlayerSheet widget

---
+++ lib/widgets/player/player_sheet.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PlayerSheet extends StatelessWidget {
+  const PlayerSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox.shrink();
+  }
+}
