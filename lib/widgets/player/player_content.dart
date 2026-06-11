04a38edb4257b3a4d9eb81435fb9d68b7016c626
wandavenus <75023651+wandavenus@users.noreply.github.com>
2026-06-12 04:20:32 +0700

    Update player_content.dart

--- lib/widgets/player/player_content.dart
+++ lib/widgets/player/player_content.dart
@@ -73,7 +73,7 @@ class PlayerContent extends StatelessWidget {
                 const SizedBox(height: 32),
                 PlayerSecondaryControls(lyrics: lyrics),
                 Transform.translate(
-                  offset: const Offset(0, -10),
+                  offset: const Offset(0, -1),
                   child: SizedBox(
                     width: MediaQuery.of(context).size.width,
                     child: const Divider(
