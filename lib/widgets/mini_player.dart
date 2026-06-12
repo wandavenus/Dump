37da38377e72a580872a4d7801e188f48fbd0aed
wandavenus <75023651+wandavenus@users.noreply.github.com>
2026-06-12 07:08:01 +0700

    Fix PlayerHeroTags import in MiniPlayer

--- lib/widgets/mini_player.dart
+++ lib/widgets/mini_player.dart
@@ -3,6 +3,7 @@ import '../services/audio_playback_state.dart';
 import '../models/local_song.dart';
 import '../services/audio_service.dart';
 import '../services/player_sheet_controller.dart';
+import 'player/player_hero_tags.dart';
 import 'song_artwork.dart';
 
 class MiniPlayer extends StatelessWidget {
