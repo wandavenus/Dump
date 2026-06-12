06882694924a77e8a6bd14b4702514001ebe9eae
wandavenus <75023651+wandavenus@users.noreply.github.com>
2026-06-12 09:15:34 +0700

    Reduce PlayerSheet to minimal render test

--- lib/widgets/player/player_sheet.dart
+++ lib/widgets/player/player_sheet.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import '../../services/audio_playback_state.dart';
-import '../../services/audio_service.dart';
-import '../../services/player_sheet_controller.dart';
-import 'player_empty_state.dart';
 
 class PlayerSheet extends StatelessWidget {
   final bool expanded;
@@ -16,67 +12,15 @@ class PlayerSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return IgnorePointer(
-      ignoring: false,
-      child: AnimatedPositioned(
-        duration: const Duration(milliseconds: 300),
-        curve: Curves.easeOutCubic,
-        left: 0,
-        right: 0,
-        top: 0,
-        bottom: 0,
-        child: Material(
-          color: const Color(0xFF000000),
-          child: ValueListenableBuilder<AudioPlaybackState>(
-            valueListenable: AudioService.playbackState,
-            builder: (context, playbackState, _) {
-              final song = playbackState.currentSong;
-
-              return Stack(
-                fit: StackFit.expand,
-                children: [
-                  const ColoredBox(color: Colors.black),
-                  SafeArea(
-                    child: Stack(
-                      children: [
-                        Positioned(
-                          top: 12,
-                          left: 0,
-                          right: 0,
-                          child: Center(
-                            child: GestureDetector(
-                              onTap: PlayerSheetController.close,
-                              child: Container(
-                                width: 36,
-                                height: 5,
-                                decoration: BoxDecoration(
-                                  color: Colors.white24,
-                                  borderRadius: BorderRadius.circular(99),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 18),
-                          child: song == null
-                              ? const PlayerEmptyState()
-                              : const Center(
-                                  child: Text(
-                                    'PLAYER SHEET WORKS',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 30,
-                                    ),
-                                  ),
-                                ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              );
-            },
+    return const Material(
+      color: Colors.red,
+      child: Center(
+        child: Text(
+          'TEST',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 50,
+            fontWeight: FontWeight.bold,
           ),
         ),
       ),
