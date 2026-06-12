367ec1d4c2d21fc77cd4e72669cf7ff5fab99b7b
wandavenus <75023651+wandavenus@users.noreply.github.com>
2026-06-12 14:59:20 +0700

    Add progress state for interactive player transition

--- lib/services/player_sheet_controller.dart
+++ lib/services/player_sheet_controller.dart
@@ -6,15 +6,24 @@ class PlayerSheetController {
   static final ValueNotifier<bool> expanded =
       ValueNotifier<bool>(false);
 
+  static final ValueNotifier<double> progress =
+      ValueNotifier<double>(0.0);
+
   static void open() {
     expanded.value = true;
+    progress.value = 1.0;
   }
 
   static void close() {
     expanded.value = false;
+    progress.value = 0.0;
   }
 
   static void toggle() {
-    expanded.value = !expanded.value;
+    if (expanded.value) {
+      close();
+    } else {
+      open();
+    }
   }
 }
