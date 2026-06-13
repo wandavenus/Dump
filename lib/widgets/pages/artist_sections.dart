18bfe194b5fa52ce35a42b836836deb48ecd903b
wandavenus <75023651+wandavenus@users.noreply.github.com>
2026-06-13 13:16:21 +0700

    Update artist_sections.dart

--- lib/widgets/pages/artist_sections.dart
+++ lib/widgets/pages/artist_sections.dart
@@ -1,24 +1,65 @@
 import 'package:flutter/material.dart';
+import '../../services/media_store_service.dart';
 
-import '../../utils/sample_music_data.dart';
-import 'detail_sections.dart';
+class ArtistListContent extends StatefulWidget {
+  const ArtistListContent({super.key});
 
-class ArtistPageContent extends StatelessWidget {
-  const ArtistPageContent({super.key, required this.currentIndex});
+  @override
+  State<ArtistListContent> createState() => _ArtistListContentState();
+}
 
-  final int currentIndex;
+class _ArtistListContentState extends State<ArtistListContent> {
+  List<String> artists = [];
+  bool loading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadArtists();
+  }
+
+  Future<void> _loadArtists() async {
+    final songs = await MediaStoreService.getSongs();
+
+    final uniqueArtists = songs
+        .map((e) => e.artist)
+        .toSet()
+        .toList()
+      ..sort();
+
+    setState(() {
+      artists = uniqueArtists;
+      loading = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    final songs = artistSongs[currentIndex];
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          ArtistHero(songs: songs),
-          const PlayShuffleButtons(),
-          SongListSection(songs: songs, showHeader: true),
-        ],
-      ),
+    if (loading) {
+      return const Center(child: CircularProgressIndicator());
+    }
+
+    if (artists.isEmpty) {
+      return const Center(child: Text('No artists found'));
+    }
+
+    return ListView.builder(
+      itemCount: artists.length,
+      itemBuilder: (context, index) {
+        final artist = artists[index];
+
+        return ListTile(
+          leading: const Icon(Icons.person),
+          title: Text(artist),
+          onTap: () {
+            Navigator.pushNamed(
+              context,
+              '/artist',
+              arguments: artist,
+            );
+          },
+        );
+      },
     );
   }
 }
