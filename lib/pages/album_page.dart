92a0994d43c7b4867d2ab2e474106fbd052c5788
wandavenus <75023651+wandavenus@users.noreply.github.com>
2026-06-13 15:00:35 +0700

    migrate album page to LocalSong arguments

--- lib/pages/album_page.dart
+++ lib/pages/album_page.dart
@@ -1,30 +1,28 @@
 import 'package:flutter/material.dart';
 
+import '../models/local_song.dart';
 import '../widgets/pages/album_sections.dart';
 
-class AlbumPage extends StatefulWidget {
+class AlbumPage extends StatelessWidget {
   const AlbumPage({super.key});
 
   @override
-  State<AlbumPage> createState() => _AlbumPageState();
-}
-
-class _AlbumPageState extends State<AlbumPage> {
-  int currentIndex = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      final routes = ModalRoute.of(context)?.settings.arguments as Map<String, int>?;
-      if (routes != null && routes.containsKey('index')) {
-        setState(() => currentIndex = routes['index']!);
-      }
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: AlbumPageContent(currentIndex: currentIndex));
+    final args =
+        ModalRoute.of(context)!.settings.arguments
+            as Map<String, dynamic>;
+
+    final album = args['album'] as LocalSong;
+    final songs = args['songs'] as List<LocalSong>;
+
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: AlbumPageContent(
+          album: album,
+          songs: songs,
+        ),
+      ),
+    );
   }
 }
