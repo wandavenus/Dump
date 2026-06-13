3d221528be0ae57d064cf19c5341478b5edaf937
wandavenus <75023651+wandavenus@users.noreply.github.com>
2026-06-13 09:18:11 +0700

    Fix edge-to-edge on app resume

--- lib/main.dart
+++ lib/main.dart
@@ -77,8 +77,32 @@ class MyScrollBehavior extends MaterialScrollBehavior {
   }
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      applyEdgeToEdge();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
