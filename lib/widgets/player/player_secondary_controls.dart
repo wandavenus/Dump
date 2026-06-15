3856a270c9b34236b438069cf23dce0e233fea42
wandavenus <75023651+wandavenus@users.noreply.github.com>
2026-06-15 20:22:51 +0700

    Remove LyricsPage import

--- lib/widgets/player/player_secondary_controls.dart
+++ lib/widgets/player/player_secondary_controls.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../../models/local_song.dart';
-import '../../pages/lyrics_page.dart';
 import '../../services/audio_playback_state.dart';
 import '../../services/audio_service.dart';
 
