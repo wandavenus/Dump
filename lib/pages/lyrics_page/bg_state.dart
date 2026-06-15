dd26b7421f72b5ef6c9afba9251895e0bdcd7bf4
wandavenus <75023651+wandavenus@users.noreply.github.com>
2026-06-15 20:04:17 +0700

    Fix missing _LyricsBackgroundState class

--- lib/pages/lyrics_page/bg_state.dart
+++ lib/pages/lyrics_page/bg_state.dart
@@ -1,20 +1,23 @@
 part of '../lyrics_page.dart';
 
-Widget build(BuildContext context) {
-  return Stack(
-    fit: StackFit.expand,
-    children: [
-      const _LyricsGradient(),
+class _LyricsBackgroundState extends State<_LyricsBackground> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        const _LyricsGradient(),
 
-      BackdropFilter(
-        filter: ImageFilter.blur(
-          sigmaX: 16,
-          sigmaY: 16,
+        BackdropFilter(
+          filter: ImageFilter.blur(
+            sigmaX: 16,
+            sigmaY: 16,
+          ),
+          child: Container(
+            color: Colors.white.withValues(alpha: 0.04),
+          ),
         ),
-        child: Container(
-          color: Colors.white.withValues(alpha: 0.04),
-        ),
-      ),
-    ],
-  );
+      ],
+    );
+  }
 }
