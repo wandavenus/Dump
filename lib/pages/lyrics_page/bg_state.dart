df5a404fdbae258c3af783f7418f067368ca4ef3
wandavenus <75023651+wandavenus@users.noreply.github.com>
2026-06-15 19:43:08 +0700

    Update bg_state.dart

--- lib/pages/lyrics_page/bg_state.dart
+++ lib/pages/lyrics_page/bg_state.dart
@@ -1,8 +1,20 @@
 part of '../lyrics_page.dart';
 
-class _LyricsBackgroundState extends State<_LyricsBackground> {
-  @override
-  Widget build(BuildContext context) {
-    return const _LyricsGradient();
-  }
+Widget build(BuildContext context) {
+  return Stack(
+    fit: StackFit.expand,
+    children: [
+      const _LyricsGradient(),
+
+      BackdropFilter(
+        filter: ImageFilter.blur(
+          sigmaX: 16,
+          sigmaY: 16,
+        ),
+        child: Container(
+          color: Colors.white.withValues(alpha: 0.02),
+        ),
+      ),
+    ],
+  );
 }
