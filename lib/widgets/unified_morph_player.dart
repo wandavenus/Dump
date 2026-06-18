80c1caf7cc1d444d3383e8eed4369cea2465019a
wandavenus <75023651+wandavenus@users.noreply.github.com>
2026-06-18 10:31:44 +0700

    Update unified_morph_player.dart

--- lib/widgets/unified_morph_player.dart
+++ lib/widgets/unified_morph_player.dart
@@ -163,7 +163,7 @@ class _UnifiedMorphPlayerState extends State<UnifiedMorphPlayer> {
 
     const miniH = 64.0;
     const miniHorizMargin = 0.0;
-    const navBarH = 55.0;
+    const navBarH = 57.0;
     const miniBottomGap = 0.0;
 
     // Eased curve for Apple-Music–like deceleration
