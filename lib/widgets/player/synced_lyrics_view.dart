71903f9bd47981e37ae97a0a583d37de6c06d064
wandavenus <75023651+wandavenus@users.noreply.github.com>
2026-06-22 19:06:37 +0700

    Update synced_lyrics_view.dart

--- lib/widgets/player/synced_lyrics_view.dart
+++ lib/widgets/player/synced_lyrics_view.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/rendering.dart';
 import '../../models/lyric_line.dart';
 import '../../models/lyrics_settings.dart';
 import '../../services/audio_service.dart';
