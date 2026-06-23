3554cca5e68229f2f0838f2f83b36e650469550a
wandavenus <75023651+wandavenus@users.noreply.github.com>
2026-06-23 08:45:28 +0700

    Update content.dart

--- lib/widgets/player/player_content/content.dart
+++ lib/widgets/player/player_content/content.dart
@@ -374,7 +374,7 @@ height: showOverlay
                   ),
 
                   Padding(
-  padding: const EdgeInsets.only(bottom: 10),
+  padding: const EdgeInsets.only(bottom: 12),
   child: Container(
     width: double.infinity,
     height: 1,
