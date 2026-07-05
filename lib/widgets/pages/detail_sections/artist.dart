50c524c94a51bc7fe839257ade3ebdeaa3c6d776
wandavenus <75023651+wandavenus@users.noreply.github.com>
2026-07-06 04:12:44 +0700

    Decrease font size of artist name text

--- lib/widgets/pages/detail_sections/artist.dart
+++ lib/widgets/pages/detail_sections/artist.dart
@@ -32,7 +32,7 @@ class ArtistHero extends StatelessWidget {
               child: Text(
                 artistName,
                 style: const TextStyle(
-                  fontSize: 42,
+                  fontSize: 36,
                   fontWeight: FontWeight.bold,
                   color: Colors.white,
                 ),
