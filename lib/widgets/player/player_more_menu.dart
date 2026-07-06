c099b6f5d392d1dbcd8e9db5c657efd02d531a3e
wandavenus <75023651+wandavenus@users.noreply.github.com>
2026-07-06 20:51:37 +0700

    Update player_more_menu.dart

--- lib/widgets/player/player_more_menu.dart
+++ lib/widgets/player/player_more_menu.dart
@@ -169,7 +169,7 @@ class PlayerMoreMenu extends StatelessWidget {
   }
 
   static IconData _loopIcon(LoopMode mode) =>
-      mode == LoopMode.one ? CupertinoIcons.loop_1 : CupertinoIcons.loop;
+      mode == LoopMode.one ? CupertinoIcons.repeat_1 : CupertinoIcons.repeat;
 
   static String _loopLabel(LoopMode mode) => switch (mode) {
     LoopMode.off => 'Off',
