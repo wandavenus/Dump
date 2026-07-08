10d214c1bb92058b396564037ff641b8ab8c5e73
wandavenus <75023651+wandavenus@users.noreply.github.com>
2026-07-08 21:18:40 +0700

    Update content.dart

--- lib/widgets/player/player_content/content.dart
+++ lib/widgets/player/player_content/content.dart
@@ -497,10 +497,10 @@ class _PlayerContentState extends State<PlayerContent> {
                                 boxShadow: [
                                   BoxShadow(
                                     color: Colors.black.withValues(
-                                      alpha: 0.0 + (0.25 * progress),
+                                      alpha: 0.0 + (0.20 * progress),
                                     ),
-                                    blurRadius: 0.0 + (2 * progress),
-                                    spreadRadius: 0.0 + (0.2 * progress),
+                                    blurRadius: 0.0 + (1 * progress),
+                                    spreadRadius: 0.0 + (0.1 * progress),
                                     offset: Offset(0, 0 + (3 * progress)),
                                   ),
                                 ],
