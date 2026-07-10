7625ba635775a422eace074259540f860113f452
wandavenus <75023651+wandavenus@users.noreply.github.com>
2026-07-10 18:13:47 +0700

    Adjust left padding in banners.dart

--- lib/widgets/pages/browse_sections/banners.dart
+++ lib/widgets/pages/browse_sections/banners.dart
@@ -17,7 +17,7 @@ class BrowseBannerCarousel extends StatelessWidget {
       height: 350,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
-        padding: const EdgeInsets.only(left: 14),
+        padding: const EdgeInsets.only(left: 18),
         itemCount: songs.length,
         itemBuilder: (context, index) {
           final song = songs[index];
