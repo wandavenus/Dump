c82bbf417e37bac8af516317929e95f32e0fa452
wandavenus <75023651+wandavenus@users.noreply.github.com>
2026-07-13 05:46:23 +0000

    web

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "672087639" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2224397199" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
