90d64cf8d19a3f4946fa8dda9bd55c8b74c96cb0
wandavenus <75023651+wandavenus@users.noreply.github.com>
2026-07-16 01:36:32 +0700

    Update state.dart

--- lib/widgets/pages/home/albums_section/state.dart
+++ lib/widgets/pages/home/albums_section/state.dart
@@ -71,7 +71,7 @@ class _LocalAlbumsSectionState extends State<_LocalAlbumsSection> {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         const Padding(
-          padding: EdgeInsets.only(left: kPageLeftPadding, top: 9),
+          padding: EdgeInsets.only(left: kPageLeftPadding, top: 11),
           child: Text(
             'Top Picks For You',
             style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
